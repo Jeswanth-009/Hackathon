cce44c22baa593e1074302a1a2acbfc4fed514d9
Jeswanth <63939480+Jeswanth-009@users.noreply.github.com>
2023-11-22 19:47:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+    alert(`Thank you, ${name}! We received your message: ${message}. We'll contact you at ${email} shortly.`);
+});
+
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(function(section) {
+        const sectionTop = section.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (sectionTop < windowHeight * 0.75) {
+            section.classList.add('active');
+        }
+    });
+});
