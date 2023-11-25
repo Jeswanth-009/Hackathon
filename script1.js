62c7af55bd92c5c3421b61f4aec5be5de08165b4
Jeswanth <63939480+Jeswanth-009@users.noreply.github.com>
2023-11-25 18:04:49 +0530

    Add files via upload

--- script1.js
+++ script1.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
       { type: 'photo', name: 'Screen', url: 'Screenshot 2023-11-23 151034.png' },
       { type: 'photo', name: 'Half', url: 'Screenshot 2023-11-23 153124.png' },
       { type: 'photo', name: 'Full', url: 'Screenshot 2023-11-23 153124.png' },
-      { type: 'video', name: 'Boiling Water', url: 'https://drive.google.com/file/d/1AhsG1BA8cPzujFOTHYUJgvTWOV9rfPJy/view?t=2s' },
+      { type: 'video', name: 'Boiling Water', url: 'https://drive.google.com/file/d/1Ae4SsSvFQan3USqlcNGVn0DluXBzDTGD/view' },
       { type: 'video', name: 'Words', url: 'https://drive.google.com/file/d/1ArZy8982XvoALwLKY-ZkGWqXcc3xZ-3m/view' }
     ];
   
